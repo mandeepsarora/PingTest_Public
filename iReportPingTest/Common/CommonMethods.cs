7054cd7dc40954b952d2fd66f9fbaccd92d6d9b0
Mandeep Arora <mandeep.s.arora@gmail.com>
2018-05-19 14:59:40 -0400

    First Checkin

---
+++ iReportPingTest/Common/CommonMethods.cs
@@ -0,0 +1,57 @@
+﻿using iReportPingTest;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRePORT_PingTest.Common
+{
+    public class CommonMethods
+    {
+        static IWebDriver m_Driver = LoginSteps.Driver;
+        private static bool isloggedIn = false;
+        public static void CaptureScreenshot()
+        {
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(m_Driver, TimeSpan.FromSeconds(10));
+            // wait.Until(driver.FindElement(By.Id("knownElementId")).Displayed);           
+
+            string scrshotpath = ConfigurationSettings.AppSettings["screenshotpath"];
+            ITakesScreenshot screenshotDriver = m_Driver as ITakesScreenshot;
+            try
+            {
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd-hhmm-ss");
+                screenshot.SaveAsFile(scrshotpath + "iRePORT_" + timestamp + ".png");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }                                                 
+        }
+
+        public static void GetURL()
+        {
+            try
+            {
+                var appurl = ConfigurationSettings.AppSettings["iRePORTURL"];
+                m_Driver.Navigate().GoToUrl(appurl);
+                m_Driver.Manage().Window.Maximize();
+                Console.WriteLine("Browser Opened");
+                System.Threading.Thread.Sleep(15000);
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+        }
+        public static void cleanup()
+        {
+            m_Driver.Quit();
+          
+        }
+    }
+}
---
+++ iReportPingTest/LoginSteps.cs
@@ -0,0 +1,59 @@
+﻿using iRePORT_PingTest.Common;
+using iRePORT_PingTest.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+using TechTalk.SpecFlow;
+
+namespace iReportPingTest
+{
+    [Binding]
+    public class LoginSteps
+    {
+        public static IWebDriver Driver = new ChromeDriver(ConfigurationSettings.AppSettings["chromeDriverPath"].ToString());
+        public LoginPage loginPage = new LoginPage(Driver);
+
+        [Given(@"I have navigated to the login screen")]
+        public void GivenIHaveNavigatedToTheLoginScreen()
+        {
+            if (Driver.PageSource.Contains("Dashboard"))
+            {
+                Console.WriteLine("I am already logged in");
+                return;
+            }
+            else
+            {
+                CommonMethods.GetURL();
+                CommonMethods.CaptureScreenshot();
+            }
+        }
+        
+        [Given(@"I enter the username and password")]
+        public void GivenIEnterTheUsernameAndPassword()
+        {
+            loginPage.EnterUsernamePassword(ConfigurationSettings.AppSettings["USER"].ToString(),
+                    ConfigurationSettings.AppSettings["PASSWORD"].ToString());
+            CommonMethods.CaptureScreenshot();
+        }
+        
+        [When(@"I click Login button")]
+        public void WhenIClickLoginButton()
+        {
+            loginPage.ClickLogin();
+            CommonMethods.CaptureScreenshot();
+        }
+        
+        [Then(@"I should see the dashboard")]
+        public void ThenIShouldSeeTheDashboard()
+        {
+            System.Threading.Thread.Sleep(50000);
+
+            NUnit.Framework.Assert.AreEqual(true, Driver.PageSource.Contains("Dashboard"));
+            // Console.WriteLine("PASS");                
+
+            CommonMethods.CaptureScreenshot();
+            CommonMethods.cleanup();
+        }
+    }
+}
